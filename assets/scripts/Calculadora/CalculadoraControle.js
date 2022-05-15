ea7836435e5e4aaab2702a1520087bf338e07cff
Lucas Manica <lucasmanica4@gmail.com>
2022-05-15 18:44:11 -0300

    feature: Adicionando evento nos botões

--- assets/scripts/Calculadora/CalculadoraControle.js
+++ assets/scripts/Calculadora/CalculadoraControle.js
@@ -5,5 +5,47 @@ export default class CalculadoraControle {
     constructor(tela = new Tela()) {
         this.tela = tela;
         new DataHora();
+        this.eventosBotoes();
+    }
+    eventosBotoes() {
+        document.querySelectorAll("#teclado button").forEach(el => {
+            el.addEventListener("click", (evento) => {
+                const target = evento.target;
+                switch (target.id) {
+                    case "zero":
+                    case "um":
+                    case "dois":
+                    case "tres":
+                    case "quatro":
+                    case "cinco":
+                    case "seis":
+                    case "sete":
+                    case "oito":
+                    case "nove":
+                        this.adicionarNumero(Number(target.dataset.valor));
+                        break;
+                    case "adicao":
+                    case "subtracao":
+                    case "multiplicacao":
+                    case "divisao":
+                        break;
+                    case "ponto":
+                        break;
+                    case "limpar":
+                        break;
+                    case "desfazer":
+                        break;
+                    case "porcentagem":
+                        break;
+                    case "igual":
+                        break;
+                    default:
+                        break;
+                }
+            });
+        });
+    }
+    adicionarNumero(numero) {
+        this.tela.conteudo = numero.toString();
     }
 }
--- assets/typescript/Calculadora/CalculadoraControle.ts
+++ assets/typescript/Calculadora/CalculadoraControle.ts
@@ -6,6 +6,49 @@ export default class CalculadoraControle {
     ) {
         
         new DataHora();
-        
+        this.eventosBotoes();
+    }
+
+    eventosBotoes(): void {
+        document.querySelectorAll("#teclado button").forEach(el => {
+            el.addEventListener("click", (evento: Event) => {
+                const target  = evento.target as HTMLButtonElement
+                switch (target.id) {
+                    case "zero":
+                    case "um":
+                    case "dois":
+                    case "tres":
+                    case "quatro":
+                    case "cinco":
+                    case "seis":
+                    case "sete":
+                    case "oito":
+                    case "nove":
+                        this.adicionarNumero(Number(target.dataset.valor));
+                        break;
+                    case "adicao":
+                    case "subtracao":
+                    case "multiplicacao":
+                    case "divisao":
+                        break;
+                    case "ponto": 
+                        break;
+                    case "limpar": 
+                        break;
+                    case "desfazer": 
+                        break;
+                    case "porcentagem":
+                        break;
+                    case "igual": 
+                        break;
+                    default:
+                        break;
+                }
+            })
+        })
+    }
+    adicionarNumero(numero: number): void {
+        this.tela.conteudo = numero.toString();
+
     }
 }
